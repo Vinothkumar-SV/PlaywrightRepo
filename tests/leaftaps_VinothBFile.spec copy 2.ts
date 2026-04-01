650ce6d555530e3fdd19cfdb24561ed65b17b9df
Vinothkumar-SV <vinothkumarsv9@gmail.com>
2026-04-01 22:47:37 +0530

    ravi Changes 25

---
+++ tests/leaftaps_VinothBFile.spec copy 2.ts
@@ -0,0 +1,26 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('http://leaftaps.com/opentaps/control/main');
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+    await page.screenshot({
+        path: `screenshots/${testInfo.title}.png`,
+        fullPage: true
+    });
+});
+
+
+
+
+
+test('has title', async ({ page }) => {
+
+    await expect(page).toHaveTitle(/Leaftaps/);
+});
+
+test('verify URL', async ({ page }) => {
+    await expect(page).toHaveURL(/opentaps/);
+    console.log("Ravi");
+});
